6fea0578b267abd447683defc1185d063e414f91
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2020-09-21 21:31:13 +0600

    Create Script.js

---
+++ break level/Script.js
@@ -0,0 +1,10 @@
+var text = "";
+var cars =["Volvo","Saab","Hero","Hero"];
+list:{
+text+=cars[0] + "<br>";
+text+=cars[1] + "<br>";
+break list;
+text+=cars[2] + "<br>";
+text+=cars[3] + "<br>;
+}
+console.log(text);
